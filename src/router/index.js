3c19707fc24935a8ebdb6a6a82be30aad1749d0b
zqn <zqn@qq.com>
2020-09-28 21:51:39 +0800

    my-mobile-pro

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    component: ()=>import("../views/index.vue")
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: ()=>import("../views/login.vue")
   }
 ]
 
@@ -24,6 +22,17 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// 路由守卫
+router.beforeEach((to,from,next)=>{
+  const islogin=localStorage.ele_login ? true:false;
+  if(to.path=="/login"){
+    next()
+  }else{
+    // 是否在登录状态下
+    islogin ? next() : next("/login")
+  }
 })
 
 export default router
---
+++ src/views/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <div class="logo">
+      <img src="../assets/logo.png" alt="my login image">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"login"
+}
+</script>
+<style>
+.login{
+    width: 100%;
+    height: 100%;
+    padding: 30px;
+    box-sizing: border-box;
+    background: #fff;
+}
+.logo{
+  text-align: center;
+}
+.logo img{
+  width: 250px;
+}
+</style>
