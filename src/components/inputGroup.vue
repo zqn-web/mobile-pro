49e89cdde06ec3e09f7eaaa2a1f7fbee6fdaf188
zqn <zqn@qq.com>
2020-10-22 19:53:53 +0800

    zqn add

---
+++ src/components/inputGroup.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="text_group">
+      <!-- 组件结构 -->
+      <!--组件容器 -->
+      <div class="input_group">
+          <!-- 输入框 -->
+          <input 
+          :type="type" 
+          :value="value" 
+          :placeholder="placeholder" 
+          :name="name" 
+          @input="$emit('input',$event.target.value)">
+      </div>
+      <!-- 输入框后面的按钮 -->
+      <button v-if="btnTitle" :disabled="disabled" @click="$emit(btnClick)">{{btnTitle}}</button>
+      <!-- 错误提醒 -->
+      <div v-if="error" class="invalid-feedback">{{error}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"inputGroup",
+    props:{
+        type:{
+            type:String,
+            default:"text"
+        },
+        value:String,
+        placeholder:String,
+        name:String,
+        btnTitle:String,
+        disabled:Boolean,
+        error:String
+    }
+}
+</script>
+<style scoped>
+  
+
+
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -3,12 +3,51 @@
     <div class="logo">
       <img src="../assets/logo.png" alt="my login image">
     </div>
+    <!-- 手机号 -->
+    <inputGroup
+     type="number"
+     v-model="phone"
+     placeholder="手机号"
+     :btnTitle="btnTitle"
+     :disabled="disabled"
+     :error="errors.phone"/>
+    <!--验证码  -->
+    <inputGroup
+     type="number"
+     v-model="verifyCode"
+     placeholder="验证码"
+     :error="errors.code"/>
+     <!-- 用户服务协议 -->
+     <div class="login_des">
+       <p>新用户登录即自动注册，表示已同意
+         <span>《用户服务协议》</span>
+       </p>
+     </div>
+     <!-- 登录按钮 -->
+     <div class="login_btn">
+       <button>登录</button>
+     </div>
   </div>
 </template>
 
 <script>
+import inputGroup from '../components/inputGroup.vue'
+
 export default {
-    name:"login"
+    name:"login",
+    data(){
+      return {
+        phone:"",//手机号码输入框
+        verifyCode:"",//验证码输入框
+        errors:{},
+        btnTitle:"获取验证码",
+        disabled:false
+      }
+
+    },
+    components:{
+      inputGroup
+    }
 }
 </script>
 <style>
